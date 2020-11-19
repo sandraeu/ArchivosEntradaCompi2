fbec47b3eb0ee3416d486adbf7106b3babf20d8e
sandraeu <45029403+sandraeu@users.noreply.github.com>
2020-11-18 18:16:58 -0600

    Add files via upload

---
+++ Matriosh_Fase2/Optimizacion/Salida/SalidaOptimizacion.ts
@@ -0,0 +1,514 @@
+/*
+	Archivo corregido de la salida de optimizacion
+	se concateno el signo '=' de las asignaciones al Heap y Stack
+*/
+
+#include <stdio.h>
+float Heap[100000];
+float Stack[100000];
+float P;
+float H;
+float t1,t2,t3,t4,t5,t6,t7,t8,t9,t10,t11,t12,t13,t14,t15,t16,t17,t18,t19,t20,t21,t22,t23,t24,t25,t26,t27,t28,t29,t30,t31,t32,t33,t34,t35,t36,t37,t38,t39,t40,t41,t42,t43,t44,t45,t46,t47,t48,t49,t50,t51,t52,t53,t54,t55,t56,t57,t58,t59,t60,t61,t62,t63,t64,t65,t66,t67,t68;
+void funcion1();
+void sandraeu_convert_tempInt_cadena();
+void sandraeu_concat_cadenas();
+void sandraeu_concatDouble();
+void sandraeu_concatBool();
+void main(){ 
+//Se cumple la Regla 10 
+ t34 = 1; 
+//Se cumple la Regla 16 
+t35 = 0; 
+t36 = t34 + t35 ; 
+//Se cumple la Regla 15 
+t37 = 0 ; 
+t38 = t36 + t37 ; 
+//Se cumple la Regla 11 
+ t39 = 1; 
+t40 = t38 - t39 ; 
+t41 = 0 - 1 ; 
+t42 = t40 + t41 ; 
+//Se cumple la Regla 14 
+t43 = 3 + 3; 
+t44 = t42 + t43 ; 
+//Se cumple la Regla 12 
+t45 = 3; 
+t46 = t44 - t45 ; 
+//Se cumple la Regla 13 
+t47 = 4 ; 
+t48 = t46 + t47 ; 
+t49 = H ;
+Heap[(int)t49]=t48 ;
+Heap[(int)t49]=t48 ;
+H = H + 1 ; 
+t56 = 0 ;
+L34: 
+t56 = 1 ;
+L35: 
+printf("%d",(int)1);
+printf("%c",(char)32);
+printf("%c",(char)10);
+//--> Se cumple la Regla 2 
+if (t56 != 1) goto L33 ;
+L33: 
+t57 = 0 ;
+L38: 
+t57 = 1 ;
+L39: 
+printf("%d",(int)2);
+printf("%c",(char)32);
+printf("%c",(char)10);
+//--> Se cumple la Regla 2 
+if (t57 != 1) goto L37 ;
+L37: 
+printf("%d",(int)3);
+printf("%c",(char)32);
+printf("%c",(char)10);
+L31: 
+t58 = 0 ;
+L43: 
+t58 = 1 ;
+L44: 
+printf("%d",(int)4);
+printf("%c",(char)32);
+printf("%c",(char)10);
+//--> Se cumple la Regla 2 
+if (t58 != 1) goto L42 ;
+L42: 
+printf("%d",(int)5);
+printf("%c",(char)32);
+printf("%c",(char)10);
+L40: 
+t59 = 0 ;
+L48: 
+t59 = 1 ;
+L49: 
+t60 = 0 ;
+L50: 
+t60 = 1 ;
+L51: 
+//--> Se cumple la Regla 2 
+if (t59 != 1) goto L52 ;
+t61 = 0 ;
+L52: 
+L54: 
+t61 = 1 ;
+L55: 
+t62 = 0 ;
+L56: 
+t62 = 1 ;
+L57: 
+//--> Se cumple la Regla 2 
+if (t61 != 1) goto L58 ;
+t63 = 0 ;
+L58: 
+L60: 
+t63 = 1 ;
+L61: 
+//--> Se cumple la Regla 2 
+if (t63 != 1) goto L47 ;
+t64 = H ;
+Heap[(int)H]=104 ;
+Heap[(int)H]=104 ;
+H = H + 1 ; 
+Heap[(int)H]=111 ;
+Heap[(int)H]=111 ;
+H = H + 1 ; 
+Heap[(int)H]=108 ;
+Heap[(int)H]=108 ;
+H = H + 1 ; 
+Heap[(int)H]=97 ;
+Heap[(int)H]=97 ;
+H = H + 1 ; 
+Heap[(int)H]=32 ;
+Heap[(int)H]=32 ;
+H = H + 1 ; 
+Heap[(int)H]=109 ;
+Heap[(int)H]=109 ;
+H = H + 1 ; 
+Heap[(int)H]=117 ;
+Heap[(int)H]=117 ;
+H = H + 1 ; 
+Heap[(int)H]=110 ;
+Heap[(int)H]=110 ;
+H = H + 1 ; 
+Heap[(int)H]=100 ;
+Heap[(int)H]=100 ;
+H = H + 1 ; 
+Heap[(int)H]=111 ;
+Heap[(int)H]=111 ;
+H = H + 1 ; 
+Heap[(int)H]=-1 ;
+Heap[(int)H]=-1 ;
+H = H + 1 ; 
+L63: 
+t65= Heap[(int)t64];
+printf("%c",(char)t65);
+t64 = t64 + 1 ; 
+L62: 
+printf("%c",(char)32);
+L64: 
+printf("%c",(char)10);
+L47: 
+t66 = H ;
+Heap[(int)H]=65 ;
+Heap[(int)H]=65 ;
+H = H + 1 ; 
+Heap[(int)H]=100 ;
+Heap[(int)H]=100 ;
+H = H + 1 ; 
+Heap[(int)H]=105 ;
+Heap[(int)H]=105 ;
+H = H + 1 ; 
+Heap[(int)H]=111 ;
+Heap[(int)H]=111 ;
+H = H + 1 ; 
+Heap[(int)H]=115 ;
+Heap[(int)H]=115 ;
+H = H + 1 ; 
+Heap[(int)H]=32 ;
+Heap[(int)H]=32 ;
+H = H + 1 ; 
+Heap[(int)H]=109 ;
+Heap[(int)H]=109 ;
+H = H + 1 ; 
+Heap[(int)H]=117 ;
+Heap[(int)H]=117 ;
+H = H + 1 ; 
+Heap[(int)H]=110 ;
+Heap[(int)H]=110 ;
+H = H + 1 ; 
+Heap[(int)H]=100 ;
+Heap[(int)H]=100 ;
+H = H + 1 ; 
+Heap[(int)H]=111 ;
+Heap[(int)H]=111 ;
+H = H + 1 ; 
+Heap[(int)H]=-1 ;
+Heap[(int)H]=-1 ;
+H = H + 1 ; 
+L66: 
+t67= Heap[(int)t66];
+printf("%c",(char)t67);
+t66 = t66 + 1 ; 
+L65: 
+printf("%c",(char)32);
+L67: 
+printf("%c",(char)10);
+L45: 
+P = P + 1 ; 
+P = P + 1 ; 
+funcion1();
+P = P - 1 ; 
+P = P - 1 ; 
+return; 
+}
+void funcion1(){ 
+L21: 
+//--> Se cumple la Regla 2 
+if (t67 != -1) goto L20 ;
+t50 = H ;
+Heap[(int)H]=104 ;
+Heap[(int)H]=104 ;
+H = H + 1 ; 
+Heap[(int)H]=111 ;
+Heap[(int)H]=111 ;
+H = H + 1 ; 
+Heap[(int)H]=108 ;
+Heap[(int)H]=108 ;
+H = H + 1 ; 
+Heap[(int)H]=97 ;
+Heap[(int)H]=97 ;
+H = H + 1 ; 
+Heap[(int)H]=32 ;
+Heap[(int)H]=32 ;
+H = H + 1 ; 
+Heap[(int)H]=109 ;
+Heap[(int)H]=109 ;
+H = H + 1 ; 
+Heap[(int)H]=117 ;
+Heap[(int)H]=117 ;
+H = H + 1 ; 
+Heap[(int)H]=110 ;
+Heap[(int)H]=110 ;
+H = H + 1 ; 
+Heap[(int)H]=100 ;
+Heap[(int)H]=100 ;
+H = H + 1 ; 
+Heap[(int)H]=111 ;
+Heap[(int)H]=111 ;
+H = H + 1 ; 
+Heap[(int)H]=-1 ;
+Heap[(int)H]=-1 ;
+H = H + 1 ; 
+L23: 
+t51= Heap[(int)t50];
+printf("%c",(char)t51);
+t50 = t50 + 1 ; 
+L22: 
+printf("%c",(char)32);
+L24: 
+printf("%c",(char)10);
+goto L20; 
+
+t52 = H ;
+Heap[(int)H]=67 ;
+Heap[(int)H]=67 ;
+H = H + 1 ; 
+Heap[(int)H]=111 ;
+Heap[(int)H]=111 ;
+H = H + 1 ; 
+Heap[(int)H]=100 ;
+Heap[(int)H]=100 ;
+H = H + 1 ; 
+Heap[(int)H]=105 ;
+Heap[(int)H]=105 ;
+H = H + 1 ; 
+Heap[(int)H]=103 ;
+Heap[(int)H]=103 ;
+H = H + 1 ; 
+Heap[(int)H]=111 ;
+Heap[(int)H]=111 ;
+H = H + 1 ; 
+Heap[(int)H]=32 ;
+Heap[(int)H]=32 ;
+H = H + 1 ; 
+Heap[(int)H]=105 ;
+Heap[(int)H]=105 ;
+H = H + 1 ; 
+Heap[(int)H]=110 ;
+Heap[(int)H]=110 ;
+H = H + 1 ; 
+Heap[(int)H]=97 ;
+Heap[(int)H]=97 ;
+H = H + 1 ; 
+Heap[(int)H]=108 ;
+Heap[(int)H]=108 ;
+H = H + 1 ; 
+Heap[(int)H]=99 ;
+Heap[(int)H]=99 ;
+H = H + 1 ; 
+Heap[(int)H]=97 ;
+Heap[(int)H]=97 ;
+H = H + 1 ; 
+Heap[(int)H]=110 ;
+Heap[(int)H]=110 ;
+H = H + 1 ; 
+Heap[(int)H]=122 ;
+Heap[(int)H]=122 ;
+H = H + 1 ; 
+Heap[(int)H]=97 ;
+Heap[(int)H]=97 ;
+H = H + 1 ; 
+Heap[(int)H]=98 ;
+Heap[(int)H]=98 ;
+H = H + 1 ; 
+Heap[(int)H]=108 ;
+Heap[(int)H]=108 ;
+H = H + 1 ; 
+Heap[(int)H]=101 ;
+Heap[(int)H]=101 ;
+H = H + 1 ; 
+Heap[(int)H]=-1 ;
+Heap[(int)H]=-1 ;
+H = H + 1 ; 
+L26: 
+t53= Heap[(int)t52];
+printf("%c",(char)t53);
+t52 = t52 + 1 ; 
+L25: 
+printf("%c",(char)32);
+L27: 
+printf("%c",(char)10);
+L20: 
+t54 = H ;
+Heap[(int)H]=97 ;
+Heap[(int)H]=97 ;
+H = H + 1 ; 
+Heap[(int)H]=100 ;
+Heap[(int)H]=100 ;
+H = H + 1 ; 
+Heap[(int)H]=105 ;
+Heap[(int)H]=105 ;
+H = H + 1 ; 
+Heap[(int)H]=111 ;
+Heap[(int)H]=111 ;
+H = H + 1 ; 
+Heap[(int)H]=115 ;
+Heap[(int)H]=115 ;
+H = H + 1 ; 
+Heap[(int)H]=-1 ;
+Heap[(int)H]=-1 ;
+H = H + 1 ; 
+L29: 
+t55= Heap[(int)t54];
+printf("%c",(char)t55);
+t54 = t54 + 1 ; 
+L28: 
+printf("%c",(char)32);
+L30: 
+printf("%c",(char)10);
+L18: 
+return; 
+}
+void sandraeu_convert_tempInt_cadena(){ 
+t1 = P + 1 ; 
+t2= Stack[(int)t1];
+t3 = t2 ;
+t4 = H ;
+t5 = 48 ;
+Heap[(int)H]=t5 ;
+Heap[(int)H]=t5 ;
+H = H + 1 ; 
+L4: 
+t2 = t2 * -1 ; 
+L1: 
+//--> Se cumple la Regla 2 
+if (t2 <= 1) goto L3 ;
+t5 = (int)t2 % 10;
+t5 = t5 + 48 ; 
+Heap[(int)H]=t5 ;
+Heap[(int)H]=t5 ;
+H = H + 1 ; 
+t2 = t2 / 10 ; 
+L3: 
+t6 = H - 1 ; 
+Heap[(int)H]=45 ;
+Heap[(int)H]=45 ;
+H = H + 1 ; 
+t6 = t6 + 1 ; 
+L7: 
+//--> Se cumple la Regla 2 
+if (t4 > t6) goto L9 ;
+t7= Heap[(int)t4];
+t8= Heap[(int)t6];
+Heap[(int)t4]=t8 ;
+Heap[(int)t4]=t8 ;
+Heap[(int)t6]=t7 ;
+Heap[(int)t6]=t7 ;
+t4 = t4 + 1 ; 
+t6 = t6 - 1 ; 
+L9: 
+L5: 
+t9 = 0 - 1 ; 
+Heap[(int)H]=t9 ;
+Heap[(int)H]=t9 ;
+H = H + 1 ; 
+return; 
+}
+void sandraeu_concat_cadenas(){ 
+t11 = P + 1 ; 
+t12= Stack[(int)t11];
+t13 = 0 - 1 ; 
+L10: 
+t10= Heap[(int)t12];
+Heap[(int)H]=t10 ;
+Heap[(int)H]=t10 ;
+H = H + 1 ; 
+t12 = t12 + 1 ; 
+L11: 
+t15 = P + 2 ; 
+t16= Stack[(int)t15];
+t17 = 0 - 1 ; 
+L12: 
+t14= Heap[(int)t16];
+Heap[(int)H]=t14 ;
+Heap[(int)H]=t14 ;
+H = H + 1 ; 
+t16 = t16 + 1 ; 
+L13: 
+t18 = -1 ;
+Heap[(int)H]=t18 ;
+Heap[(int)H]=t18 ;
+H = H + 1 ; 
+return; 
+}
+void sandraeu_concatDouble(){ 
+t19 = P + 1 ; 
+t20= Stack[(int)t19];
+t21 = (int)t20 ;
+t22 = t20 - t21 ; 
+t23 = t22 * 100000 ; 
+t23 = (int)t23 ;
+t23 = t23 * -1 ; 
+L14: 
+//Se cumple la Regla 10 
+ t24 = P; 
+t25 = t24 + 1 ; 
+Stack[(int)t25]=t21 ;
+Stack[(int)t25]=t21 ;
+t26 = H ;
+/*Se cumple regla 6 
+   P = P + 0; 
+<codigo eliminado> */ 
+
+sandraeu_convert_tempInt_cadena();
+/*Se cumple regla 7 
+   P = P - 0; 
+<codigo eliminado> */ 
+
+H = H - 1 ; 
+Heap[(int)H]=46 ;
+Heap[(int)H]=46 ;
+H = H + 1 ; 
+//Se cumple la Regla 10 
+ t27 = P; 
+t28 = t27 + 1 ; 
+Stack[(int)t28]=t23 ;
+Stack[(int)t28]=t23 ;
+t29 = H ;
+/*Se cumple regla 6 
+   P = P + 0; 
+<codigo eliminado> */ 
+
+sandraeu_convert_tempInt_cadena();
+/*Se cumple regla 7 
+   P = P - 0; 
+<codigo eliminado> */ 
+
+t30 = 0 - 1 ; 
+Heap[(int)H]=t30 ;
+Heap[(int)H]=t30 ;
+H = H + 1 ; 
+return; 
+}
+void sandraeu_concatBool(){ 
+t31 = P + 1 ; 
+t32= Stack[(int)t31];
+//--> Se cumple la Regla 2 
+if (t32 != 1) goto L17 ;
+Heap[(int)H]=116 ;
+Heap[(int)H]=116 ;
+H = H + 1 ; 
+Heap[(int)H]=114 ;
+Heap[(int)H]=114 ;
+H = H + 1 ; 
+Heap[(int)H]=117 ;
+Heap[(int)H]=117 ;
+H = H + 1 ; 
+Heap[(int)H]=101 ;
+Heap[(int)H]=101 ;
+H = H + 1 ; 
+L17: 
+Heap[(int)H]=102 ;
+Heap[(int)H]=102 ;
+H = H + 1 ; 
+Heap[(int)H]=97 ;
+Heap[(int)H]=97 ;
+H = H + 1 ; 
+Heap[(int)H]=108 ;
+Heap[(int)H]=108 ;
+H = H + 1 ; 
+Heap[(int)H]=115 ;
+Heap[(int)H]=115 ;
+H = H + 1 ; 
+Heap[(int)H]=101 ;
+Heap[(int)H]=101 ;
+H = H + 1 ; 
+L15: 
+t33 = 0 - 1 ; 
+Heap[(int)H]=t33 ;
+Heap[(int)H]=t33 ;
+H = H + 1 ; 
+return; 
+}
